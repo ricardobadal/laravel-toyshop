d0e2ecc5b065afd6f0678d239854532612065539
MahnazJ <md.jazayeri.sev@gmail.com>
2022-03-07 11:21:30 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 <div><h1>Home page</h1></div>
 
 <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-    <p><i>I Guess this is going to be the main/home page</i></p>
+    <p><i>main/home page</i></p>
 </div>
 
 @endsection
