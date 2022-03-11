f45b7ff32df48be7121afcb8ba8fea11f8390e34
MahnazJ <md.jazayeri.sev@gmail.com>
2022-03-11 12:58:36 +0100

    added footer to app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -148,9 +148,18 @@
         </main>
     </div>
 
-    <div>
-        <footer>This is our Footer</footer>
-    </div>
+    <div class="container-fluid">
+        <div class="row mt-5">
+            <div class="col footer p-3 text-center">
+                <footer>
+
+                    <p> &copy; Copyright 2022 by ToyShop. All rights reserved.</p>
+
+
+                </footer>
+            </div>
+        </div>
+</div>
 </body>
 </html>
 
